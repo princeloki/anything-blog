b1651aa88a736fda4b4846e5e46bb6285380e858
princeloki <teriqstrachan@yahoo.com>
2023-02-08 01:56:02 -0800

    Finished setting up login and register css, as well as react handleForm logic

--- blog/client/src/pages/Login.jsx
+++ blog/client/src/pages/Login.jsx
@@ -1,23 +1,45 @@
 import React from "react"
 import Nav from './components/Nav'
+import {useState} from "react"
 
 function Login(){
+    const [checked, setChecked] = useState(false)
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    })
+
+    function handleChange(e){
+        setFormData(prevFormData=>{
+            return{
+                ...prevFormData,
+                [e.target.name]: e.target.value,
+            }
+        })
+    }
+
+    function handleSubmit(){
+        return(
+            console.log(formData)
+        )
+    }
+
     return(
         <div className="app">
             <div className="form-page">
                 <div className="container">
                     <h1>Sign In</h1>
                     <p>Don't have an account yet? <a href="register">Sign Up</a></p>
-                    <form onSubmit="">
+                    <form onSubmit={handleSubmit}>
                         <label htmlFor="email">Email Address</label>
-                        <input id = "email" name="email" type="email" placeholder="..Enter your email address please"/>
+                        <input id = "email" name="email" type="email" value={formData.email} onChange={(e)=>handleChange(e)}  placeholder="..Enter your email address please"/>
                         <div className="pass-label">
                             <label htmlFor="password">Password</label>
                             <a href="#">Forgot password?</a>
                         </div>
-                        <input id = "password" name="password" type="password" placeholder="..Enter your password please"/>
+                        <input id = "password" name="password" type="password" value={formData.password} onChange={(e)=>handleChange(e)}  placeholder="..Enter your password please"/>
                         <div className="check">
-                            <input id="checkbox" type="checkbox" />
+                            <input className="checkbox" type="checkbox" />
                             <label id="check-label" htmlFor="checkbox">Remember me!</label>
                         </div>
                         <button type="submit">Login</button>
@@ -40,3 +62,4 @@ function Login(){
 }
 
 export default Login
+
--- blog/client/src/pages/Register.jsx
+++ blog/client/src/pages/Register.jsx
@@ -1,20 +1,52 @@
 import React from "react"
 import Nav from './components/Nav'
+import {useState} from 'react'
 
 function Login(){
+    const [formData, setFormData] = useState({
+        option: '',
+        email: '',
+        username: '',
+        password: '',
+    })
+    
+    function handleChange(e){
+        console.log(formData)
+        setFormData(prevFormData=>{
+            return{
+                ...prevFormData,
+                [e.target.name]: e.target.value,
+            }
+        })
+    }
+
+    function handleSubmit(){
+        return(
+            console.log(formData)
+        )
+    }
+
     return(
         <div className="app">
             <div className="form-page">
                 <div className="container">
                     <h1>Sign Up</h1>
                     <p>Already have an account? <a href="login">Sign In</a></p>
-                    <form onSubmit="">
+                    <form onSubmit={handleSubmit}>
+                        <label>
+                            Choose account type
+                            <select className="select" onChange={(e)=>handleChange(e)}>
+                                <option>--Choose Account Type--</option>
+                                <option value="Reader">Reader</option>
+                                <option value="Creator">Creator</option>
+                            </select>
+                        </label>
                         <label htmlFor="email">Email Address</label>
-                        <input id = "email" name="email" type="email" placeholder="..Enter email address here"/>
+                        <input id = "email" name="email" type="email" value={formData.email} onChange={(e)=>handleChange(e)} placeholder="..Enter email address here"/>
                         <label htmlFor="username">Username</label>
-                        <input id = "username" name="username" type="username" placeholder="..Enter username here"/>
+                        <input id = "username" name="username" type="username" value={formData.username} onChange={(e)=>handleChange(e)}  placeholder="..Enter username here"/>
                         <label htmlFor="password">Password</label>
-                        <input id = "password" name="password" type="password" placeholder="..Enter password here"/>
+                        <input id = "password" name="password" type="password" value={formData.password} onChange={(e)=>handleChange(e)}  placeholder="..Enter password here"/>
                         <label htmlFor="password">Confirm Password</label>
                         <input id = "second_password" name="password" type="password" placeholder="..Enter password here"/>
 
